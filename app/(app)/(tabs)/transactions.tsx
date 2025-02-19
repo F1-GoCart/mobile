843c829cde13f447bfc4bb0d4852e533eaf6996c
Ryan Caezar Itang <sitiom@proton.me>
2025-02-19 23:45:38 +0800

    feat: connect db data to transactions

--- app/(app)/(tabs)/transactions.tsx
+++ app/(app)/(tabs)/transactions.tsx
@@ -1,8 +1,10 @@
-import { View } from "react-native";
+import { FlatList, RefreshControl, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import useAuthStore from "~/stores/AuthStore";
 import { Calendar } from "~/lib/icons/Calendar";
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import { supabase } from "~/lib/supabase";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Screen() {
   const { session } = useAuthStore();
@@ -11,42 +13,64 @@ export default function Screen() {
     return null;
   }
 
+  const {
+    data: transactions,
+    status,
+    error,
+    isRefetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["transactions", session.user.id],
+    queryFn: async () => {
+      const { data, error } = await supabase
+        .from("purchase_history")
+        .select()
+        .eq("user_id", session.user.id);
+
+      if (error) {
+        throw error;
+      }
+
+      return data;
+    },
+  });
   return (
-    <View className="flex-1 items-center gap-9 bg-secondary/30 px-12 py-5">
-      <View className="flex-row items-center gap-3">
-        <Avatar alt="Zach Nugent's Avatar" className="flex-">
-          <AvatarFallback>
-            <Text>C1</Text>
-          </AvatarFallback>
-        </Avatar>
-        <View className="w-full flex-row items-center justify-between">
-          <View>
-            <Text className="font-bold">Cart 1</Text>
+    <FlatList
+      data={transactions}
+      className="w-full flex-1 bg-secondary/20 p-5"
+      keyExtractor={(item) => item.id}
+      refreshControl={
+        <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+      }
+      ItemSeparatorComponent={() => (
+        <View className="my-5 h-[0.5] w-full bg-[#d6dfd7]" />
+      )}
+      renderItem={({ item }) => (
+        <View className="w-full flex-row items-center gap-3">
+          <Avatar alt="Cart id">
+            <AvatarFallback>
+              <Text>{`C${item.cart_id}`}</Text>
+            </AvatarFallback>
+          </Avatar>
+          <View className="flex-1">
+            <Text className="font-bold">{`Cart ${item.cart_id}`}</Text>
             <View className="flex-row items-center gap-2">
-              <Calendar size={15} color="black" />
-              <Text>2024-09-23</Text>
+              <Calendar size={15} color="gray" />
+              <Text className="text-gray-500">
+                {new Date(item.datetime!).toLocaleString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                })}
+              </Text>
             </View>
           </View>
-          <Text className="font-bold">₱448.30</Text>
+          <Text className="text-right font-bold">{`₱${item.total_price!.toFixed(2)}`}</Text>
         </View>
-      </View>
-      <View className="flex-row items-center gap-3">
-        <Avatar alt="Zach Nugent's Avatar" className="flex-">
-          <AvatarFallback>
-            <Text>C1</Text>
-          </AvatarFallback>
-        </Avatar>
-        <View className="w-full flex-row items-center justify-between">
-          <View>
-            <Text className="font-bold">Cart 2</Text>
-            <View className="flex-row items-center gap-2">
-              <Calendar size={15} color="black" />
-              <Text>2021-10-18</Text>
-            </View>
-          </View>
-          <Text className="font-bold">₱563.19</Text>
-        </View>
-      </View>
-    </View>
+      )}
+    />
   );
 }
