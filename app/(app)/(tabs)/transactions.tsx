6788ca047bf987d363bc8d14803aa112ad7257e2
Ryan Caezar Itang <sitiom@proton.me>
2025-02-19 23:57:03 +0800

    feat: add payment mode icons

--- app/(app)/(tabs)/transactions.tsx
+++ app/(app)/(tabs)/transactions.tsx
@@ -2,9 +2,10 @@ import { FlatList, RefreshControl, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import useAuthStore from "~/stores/AuthStore";
 import { Calendar } from "~/lib/icons/Calendar";
-import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { supabase } from "~/lib/supabase";
 import { useQuery } from "@tanstack/react-query";
+import { CreditCard } from "~/lib/icons/CreditCard";
 
 export default function Screen() {
   const { session } = useAuthStore();
@@ -48,8 +49,17 @@ export default function Screen() {
       renderItem={({ item }) => (
         <View className="w-full flex-row items-center gap-3">
           <Avatar alt="Cart id">
+            {item.mode_of_payment === "gcash" && (
+              <AvatarImage
+                source={require("~/assets/images/logos/gcash.jpg")}
+              />
+            )}
             <AvatarFallback>
-              <Text>{`C${item.cart_id}`}</Text>
+              {item.mode_of_payment === "card" ? (
+                <CreditCard size={20} color="gray" />
+              ) : (
+                <Text>{`C${item.cart_id}`}</Text>
+              )}
             </AvatarFallback>
           </Avatar>
           <View className="flex-1">
