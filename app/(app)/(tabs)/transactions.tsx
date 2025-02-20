63a407afc93384410fa028b4118ee62f609f2edc
Ryan Caezar Itang <sitiom@proton.me>
2025-02-20 12:17:27 +0800

    chore: add pressable to transaction item

--- app/(app)/(tabs)/transactions.tsx
+++ app/(app)/(tabs)/transactions.tsx
@@ -1,4 +1,4 @@
-import { FlatList, RefreshControl, View } from "react-native";
+import { FlatList, Pressable, RefreshControl, View } from "react-native";
 import { Text } from "~/components/ui/text";
 import useAuthStore from "~/stores/AuthStore";
 import { Calendar } from "~/lib/icons/Calendar";
@@ -6,6 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { supabase } from "~/lib/supabase";
 import { useQuery } from "@tanstack/react-query";
 import { CreditCard } from "~/lib/icons/CreditCard";
+import { toast } from "sonner-native";
 
 export default function Screen() {
   const { session } = useAuthStore();
@@ -38,47 +39,55 @@ export default function Screen() {
   return (
     <FlatList
       data={transactions}
-      className="w-full flex-1 bg-secondary/20 p-5"
+      className="w-full flex-1 bg-secondary/20"
       keyExtractor={(item) => item.id}
       refreshControl={
         <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
       }
       ItemSeparatorComponent={() => (
-        <View className="my-5 h-[0.5] w-full bg-[#d6dfd7]" />
+        <View className="m-auto h-[0.5] w-[90%] bg-[#d6dfd7]" />
       )}
       renderItem={({ item }) => (
-        <View className="w-full flex-row items-center gap-3">
-          <Avatar alt="Cart id">
-            {item.mode_of_payment === "gcash" && (
-              <AvatarImage
-                source={require("~/assets/images/logos/gcash.jpg")}
-              />
-            )}
-            <AvatarFallback>
-              {item.mode_of_payment === "card" ? (
-                <CreditCard size={20} color="gray" />
-              ) : (
-                <Text>{`C${item.cart_id}`}</Text>
-              )}
-            </AvatarFallback>
-          </Avatar>
-          <View className="flex-1">
-            <Text className="font-bold">{`Cart ${item.cart_id}`}</Text>
-            <View className="flex-row items-center gap-2">
-              <Calendar size={15} color="gray" />
-              <Text className="text-gray-500">
-                {new Date(item.datetime!).toLocaleString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric",
-                  hour: "numeric",
-                  minute: "numeric",
-                  hour12: true,
-                })}
-              </Text>
+        <View className="m-2 overflow-hidden rounded-xl">
+          <Pressable
+            onPress={() => toast.success("Transaction details")}
+            android_ripple={{ color: "gray" }}
+            className="p-3"
+          >
+            <View className="w-full flex-row items-center gap-3">
+              <Avatar alt="Cart id">
+                {item.mode_of_payment === "gcash" && (
+                  <AvatarImage
+                    source={require("~/assets/images/logos/gcash.jpg")}
+                  />
+                )}
+                <AvatarFallback>
+                  {item.mode_of_payment === "card" ? (
+                    <CreditCard size={20} color="gray" />
+                  ) : (
+                    <Text>{`C${item.cart_id}`}</Text>
+                  )}
+                </AvatarFallback>
+              </Avatar>
+              <View className="flex-1">
+                <Text className="font-bold">{`Cart ${item.cart_id}`}</Text>
+                <View className="flex-row items-center gap-2">
+                  <Calendar size={15} color="gray" />
+                  <Text className="text-gray-500">
+                    {new Date(item.datetime!).toLocaleString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,
+                    })}
+                  </Text>
+                </View>
+              </View>
+              <Text className="text-right font-bold">{`₱${item.total_price!.toFixed(2)}`}</Text>
             </View>
-          </View>
-          <Text className="text-right font-bold">{`₱${item.total_price!.toFixed(2)}`}</Text>
+          </Pressable>
         </View>
       )}
     />
